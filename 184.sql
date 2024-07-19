c76254271c8cb2f99718ad8d413d39a6a4488012
thmcheung <thmarcus.cheung@mail.utoronto.ca>
2024-07-19 16:06:39 -0400

    sql

---
+++ 184.sql
@@ -0,0 +1,7 @@
+select Department.name AS Department,Employee.name AS Employee,Salary
+from Employee JOIN Department ON Employee.DepartmentId = Department.id
+where 
+(Employee.DepartmentID, Salary) in 
+(select departmentID, MAX(salary) 
+from Employee 
+Group by DepartmentId);
