4ea05d0c04d6adf54b51564e87b48bb4cd8ca56f
Ting Ho Marcus Cheung <thmarcus.cheung@mail.utoronto.ca>
2025-01-23 14:52:56 -0500

    amzn

---
+++ 1942.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+        for i in range(len(times)):
+            times[i].append(i)
+        times.sort()
+        ar = [0] * len(times)
+        ans = [-1] * len(times)
+        for i in range(len(times)):
+            for j in range(len(times)):
+                if ar[j] <= times[i][0]:
+                    ar[j] = times[i][1]
+                    ans[times[i][2]] = j
+                    break
+        return ans[targetFriend]
