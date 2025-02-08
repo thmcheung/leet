1fa9e87fc674f18b2720e6b515ea84be8d8716d0
Ting Ho Marcus Cheung <thmarcus.cheung@mail.utoronto.ca>
2025-02-08 16:39:12 -0500

    OK

---
+++ 2297.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minCost(self, nums: List[int], costs: List[int]) -> int:
+        n = len(nums)
+        dp = [float('inf')] * n
+        dp[0] = 0
+        for i in range(n):
+            flag = 0 #1: k < i; 2: k >= i
+            for j in range(i + 1, n):
+                if j - i == 1:
+                    if dp[j] > costs[j] + dp[i]:
+                        dp[j] = costs[j] + dp[i]
+                    if nums[i] > nums[j]:
+                        flag = 1
+                    else:
+                        flag = 2
+                if (nums[i] <= nums[j] and flag == 1) or (nums[i] > nums[j] and flag == 2):
+                    if dp[j] > costs[j] + dp[i]:
+                        dp[j] = costs[j] + dp[i]
+                    break
+        return dp[n-1]
