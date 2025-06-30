fa10793caf3c9f33636da49cf4d324664cda6fd4
Ting Ho Marcus Cheung <thmarcus.cheung@mail.utoronto.ca>
2025-06-30 11:46:21 -0400

    another easy question

---
+++ 283.py
@@ -0,0 +1,11 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left, right = 0, 0
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
