c6ffc2717e98727bae46f756b7834f09ecd7457e
Ting Ho Marcus Cheung <thmarcus.cheung@mail.utoronto.ca>
2025-11-16 23:09:18 -0500

    more java

---
+++ 111.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ *  public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+import java.util.*;
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int height = 1;
+        
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+                
+                // If we found a leaf node, return the current height
+                if (node.left == null && node.right == null) {
+                    return height;
+                }
+                
+                // Add children to the queue
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            height++;
+        }
+        return height;
+    }
+}
